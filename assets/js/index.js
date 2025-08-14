6dfc62b66daf1c4c0c4be8db34c46ebdb65b2315
Jamshid <jamshid_tojiboyev@mail.ru>
2025-08-14 22:21:58 +0500

    added

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+const block2 = new Swiper('.swiper-block2', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  loop: true,
+
+  pagination: {
+    el: '.pagination-block2',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.next-block2',
+    prevEl: '.prev-block2',
+  },
+
+  breakpoints: {
+    0: {
+       
+    },
+}
+});
+
+
+const block5 = new Swiper('.swiper-block5', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  loop: true,
+  
+  pagination: {
+    el: '.pagination-block5',
+    clickable: true,
+  },
+
+
+  breakpoints: {
+    0: {
+       
+    },
+}
+});
